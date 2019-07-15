b373c57012831a539f4eea5ec68153de85265e1f
Your Name <you@example.com>
2019-07-15 10:19:17 +0700

    edit Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -8,41 +8,60 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link href="{{ URL::asset('css/login.css') }}" rel="stylesheet" media="screen">
   </head>
   <body>
-  <div class="container">
-		<div class="row">
-			<div class="col-md-6 col-md-offset-3">
-				<form action="{{url('login')}}" method="POST" role="form">
-					<legend>Login</legend>
-					@if($errors->has('errorlogin'))
-						<div class="alert alert-danger">
-							<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-							{{$errors->first('errorlogin')}}
-						</div>
-					@endif
-					<div class="form-group">
-						<label for="">Username</label>
-						<input type="text" class="form-control" id="username" placeholder="Username" name="username" value="{{old('username')}}">
-						@if($errors->has('username'))
-							<p style="color:red">{{$errors->first('username')}}</p>
-						@endif
-					</div>
-					<div class="form-group">
-						<label for="">Password</label>
-						<input type="password" class="form-control" id="password" placeholder="Password" name="password">
-						@if($errors->has('password'))
-							<p style="color:red">{{$errors->first('password')}}</p>
-						@endif
-					</div>
-				
-					
-					{!! csrf_field() !!}
-					<button type="submit" class="btn btn-primary">Đăng nhập</button>
-				</form>
+	<div class="container" style="margin-top: 10%">
+	<div class="row">
+		<div class="col-sm-6 col-md-4 col-md-offset-4">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<form role="form" action="{{ url('/login') }}" method="post">
+						<fieldset>
+							<div class="row">
+								<div class="center-block">
+									<img class="profile-img" src="{{ url('images/user-icon.jpg') }}" alt="User"/>
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-12 col-md-10  col-md-offset-1 ">
+                                    @if($errors->has('errorlogin'))
+						                <div class="alert alert-danger">
+							                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+							                {{$errors->first('errorlogin')}}
+						                </div>
+					                @endif
+									<div class="form-group">
+										<div class="input-group">
+											<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span> 
+											<input class="form-control" id="username" placeholder="Username" name="username" type="text" value="{{ old('username') }}">
+										</div>
+                                        @if($errors->has('username'))
+							                <p style="color:red">{{$errors->first('username')}}</p>
+						                @endif
+									</div>
+									<div class="form-group">
+										<div class="input-group">
+											<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+											<input class="form-control" id="password" placeholder="Password" name="password" type="password" value="">
+										</div>
+                                        @if($errors->has('password'))
+							                <p style="color:red">{{$errors->first('password')}}</p>
+						                @endif
+									</div>
+                                    {!! csrf_field() !!}
+									<div class="form-group">
+										<input type="submit" class="btn btn-lg btn-primary btn-block" value="Đăng nhập">
+									</div>
+								</div>
+							</div>
+						</fieldset>
+					</form>
+				</div>
 			</div>
 		</div>
 	</div>
+</div>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
